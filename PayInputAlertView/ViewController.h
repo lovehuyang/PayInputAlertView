20751fa4f1a79c682823cc63df9bf5fc17307947
lovehuyang90@163.com <lovehuyang90@163.com>
2017-12-13 09:30:39 +0800

    Initial Commit

---
+++ PayInputAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PayInputAlertView
+//
+//  Created by tzsoft on 2017/12/13.
+//  Copyright © 2017年 HLY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PayInputAlertViewTests/PayInputAlertViewTests.m
@@ -0,0 +1,39 @@
+//
+//  PayInputAlertViewTests.m
+//  PayInputAlertViewTests
+//
+//  Created by tzsoft on 2017/12/13.
+//  Copyright © 2017年 HLY. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PayInputAlertViewTests : XCTestCase
+
+@end
+
+@implementation PayInputAlertViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
